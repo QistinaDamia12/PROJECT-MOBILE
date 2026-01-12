8f074afc4eeb84d03d990a60a734c21c0c9ffe8a
damio <2024771469@uitm.edu.my>
2026-01-12 09:38:11 +0800

    role update

--- app/src/main/java/com/example/facility_bookuitm/LoginPage.java
+++ app/src/main/java/com/example/facility_bookuitm/LoginPage.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -28,6 +29,7 @@ public class LoginPage  extends AppCompatActivity
 {
     private EditText etID;
     private EditText etPassword;
+    private Spinner spRole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -45,6 +47,7 @@ public class LoginPage  extends AppCompatActivity
         // get references to form elements
         etID = findViewById(R.id.etID);
         etPassword = findViewById(R.id.etPassword);
+        spRole = findViewById(R.id.spRole);
 
     }
 
@@ -53,9 +56,11 @@ public class LoginPage  extends AppCompatActivity
         // get username/email and password entered by user
         String userid = etID.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
+        Spinner role = findViewById(R.id.spRole);
+
 
         // validate form
-        if (validateLogin(userid, password)) {
+        if (validateLogin(userid, password, role)) {
             doLogin(userid, password);
         }
     }
@@ -83,7 +88,8 @@ public class LoginPage  extends AppCompatActivity
                         SharedPrefManager spm = new SharedPrefManager(getApplicationContext());
                         spm.storeUser(user);
 
-                        // go to MainActivity
+                        //go to user_dashboard
+                        //go to admin dashboard
                         finish();
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
@@ -111,7 +117,7 @@ public class LoginPage  extends AppCompatActivity
         });
     }
 
-    private boolean validateLogin(String userid, String password) {
+    private boolean validateLogin(String userid, String password, Spinner role) {
         if (userid == null || userid.isEmpty()) {
             Toast.makeText(this, "Email is required", Toast.LENGTH_LONG).show();
             return false;
