e758029ed5716823c3fd35080386793ce37b15b7
damio <2024771469@uitm.edu.my>
2026-01-27 16:49:50 +0800

    faci form

--- app/src/main/java/com/example/facility_bookuitm/admin_add_facility.java
+++ app/src/main/java/com/example/facility_bookuitm/admin_add_facility.java
@@ -2,15 +2,34 @@ package com.example.facility_bookuitm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.facility_bookuitm.model.User;
+import com.example.facility_bookuitm.remote.ApiUtils;
+import com.example.facility_bookuitm.remote.FacilityService;
+import com.example.facility_bookuitm.sharedpref.SharedPrefManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class admin_add_facility extends AppCompatActivity {
 
-    private ImageView imagePreview;
+    private static ImageView imagePreview;
+    private EditText txtName;
+    private EditText txtLoca;
+    private EditText txtStatus;
+    private EditText txtType;
+    private EditText txtCapacity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,12 @@ public class admin_add_facility extends AppCompatActivity {
 
         Button btnUploadImage = findViewById(R.id.btnUploadImage);
         imagePreview = findViewById(R.id.imagePreview);
+        // get view objects references
+        txtName = findViewById(R.id.txtName);
+        txtLoca = findViewById(R.id.txtLoca);
+        txtStatus = findViewById(R.id.txtStatus);
+        txtType = findViewById(R.id.txtType);
+        txtCapacity = findViewById(R.id.txtCapacity);
 
         //add image to display
         btnUploadImage.setOnClickListener(v -> showImagePicker());
@@ -29,7 +54,6 @@ public class admin_add_facility extends AppCompatActivity {
             finish(); // closes current activity and returns to previous
             overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
         });
-
 
     }
 
@@ -67,4 +91,77 @@ public class admin_add_facility extends AppCompatActivity {
     }
 
 
+    public void addFacility(View v) {
+        // get values in form
+        String name = txtName.getText().toString();
+        String location = txtLoca.getText().toString();
+        String status = txtStatus.getText().toString();
+        String type = txtType.getText().toString();
+
+        //convert text to int
+        int capacity = 0;
+        try {
+            capacity = Integer.parseInt(txtCapacity.getText().toString().trim());
+        } catch (NumberFormatException e) {
+            txtCapacity.setError("Enter a valid number");
+            return; // stop further processing
+        }
+
+        // get user info from SharedPreferences
+        SharedPrefManager spm = new SharedPrefManager(getApplicationContext());
+        User user = spm.getUser();
+
+        // send request to add new book to the REST API
+        FacilityService facilityService = ApiUtils.getFacilityService();
+        Call<Facility> call = facilityService.addFacility(name, location, "default.jpg", status, type, capacity);
+
+        // execute
+        call.enqueue(new Callback<Facility>() {
+            @Override
+            public void onResponse(Call<Facility> call, Response<Facility> response) {
+
+                // for debug purpose
+                Log.d("MyApp:", "Response: " + response.raw().toString());
+
+                if (response.code() == 201) {
+                    // book added successfully
+                    Facility addedFacility = response.body();
+                    // display message
+                    Toast.makeText(getApplicationContext(),
+                            addedFacility.getName() + " added successfully.",
+                            Toast.LENGTH_LONG).show();
+
+                    // end this activity and go back to previous activity, BookListActivity
+                    finish();
+                }
+                else if (response.code() == 401) {
+                    // invalid token, ask user to relogin
+                    Toast.makeText(getApplicationContext(), "Invalid session. Please login again", Toast.LENGTH_LONG).show();
+                    clearSessionAndRedirect();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Error: " + response.message(), Toast.LENGTH_LONG).show();
+                    // server return other error
+                    Log.e("MyApp: ", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Facility> call, Throwable throwable) {
+                Toast.makeText(getApplicationContext(),"Error [" + t.getMessage() + "]",
+                        Toast.LENGTH_LONG).show();
+                // for debug purpose
+                Log.d("MyApp:", "Error: " + t.getCause().getMessage());
+
+            }
+
+            @Override
+            public void onFailure(Call<Facility> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),"Error [" + t.getMessage() + "]",
+                        Toast.LENGTH_LONG).show();
+                // for debug purpose
+                Log.d("MyApp:", "Error: " + t.getCause().getMessage());
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/facility_bookuitm/remote/FacilityService.java
+++ app/src/main/java/com/example/facility_bookuitm/remote/FacilityService.java
@@ -1,4 +1,33 @@
 package com.example.facility_bookuitm.remote;
 
-public interface FacilityService {
+import com.example.facility_bookuitm.Facility;
+import com.example.facility_bookuitm.model.DeleteResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface FacilityService
+{
+        @GET("facility")
+        Call<List<Facility>> getAllFacility(@Header("api-key") String api_key);
+        @GET("facility/{id}")
+        Call<Facility> getFacility(@Header("api-key") String api_key, @Path("facilityID") int facilityID);
+
+        @FormUrlEncoded
+        @POST("facility")
+        Call<Facility> addFacility(@Field("facilityName") String facilityName,
+                               @Field("facilityLocation") String facilityLocation, @Field("facilityPicture") String facilityPicture,
+                               @Field("facilityStatus") String facilityStatus, @Field("facilityType") String facilityType,
+                               @Field("facilityCapacity") int facilityCapacity);
+
+        @DELETE("facility/{id}")
+        Call<DeleteResponse> deleteFacility(@Header ("api-key") String apiKey, @Path("facilityID") int facilityID);
 }
